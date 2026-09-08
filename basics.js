2455774ae42db7eb553473149938ed40f6bd095b
Aravind112703 <aravindmathi93@gmail.com>
2026-09-08 20:09:23 +0530

    variables

---
+++ basics.js
@@ -0,0 +1,32 @@
+
+// print values
+
+
+console.log("Playwright is a popular automation tool")
+
+console.log("JavaScript is a versatile programming language")
+
+
+// single line comment
+
+
+//console.log("This is a single line comment")
+
+// multi line comment
+
+/*
+
+console.log("Playwright is a popular automation tool")
+
+console.log("JavaScript is a versatile programming language")
+
+console.log("Playwright is a popular automation tool")
+*/
+console.log("JavaScript is a versatile programming language")
+
+console.log("Playwright is a popular automation tool")
+
+console.log("JavaScript is a versatile programming language")
+
+
+
---
+++ variables.js
@@ -0,0 +1,68 @@
+
+
+// variables - var, let, const
+
+
+// var - old way of declaring variables - global scope by default
+
+// let and const - modern way of declaring variables - blocked scope by default
+
+
+
+// var automation = 'playwright'
+
+// var automation = "cypress"
+
+// console.log(automation)
+
+
+// let value = "cypress"
+
+// value = "webdriver"
+
+// value = "playwright"
+
+// console.log(value)
+
+
+// const legacyTool = "selenium"
+
+// // legacyTool = "webdriver"
+
+// console.log(legacyTool)
+
+
+
+
+// case sensitive
+
+
+
+// const automation = "playwright"
+
+// console.log(automation)
+
+
+// const Automation = "cypress"
+// console.log(Automation)
+
+
+
+// camel case
+
+// first name
+
+const firstName = "john"
+
+
+// variables, functions and objects
+
+
+// Pascal - class
+
+const FirstName = "John"
+
+
+// snake case
+
+const first_name = "jhon"
